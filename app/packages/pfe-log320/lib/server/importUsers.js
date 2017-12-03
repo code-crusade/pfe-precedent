87326594c875f62c6b0272f9a3916a39b6dd0684
Kamal Jr. El Chedraoui <cilaf.93@gmail.com>
2017-12-03 13:03:01 -0500

    #43 The accounts now use the emaila ddress as the default password

--- app/packages/pfe-log320/lib/server/importUsers.js
+++ app/packages/pfe-log320/lib/server/importUsers.js
@@ -2,42 +2,41 @@ import Users from 'meteor/vulcan:users';
 import { newMutation } from 'meteor/vulcan:core';
 import XLSX from 'xlsx';
 
-const createUser = function ({ firstName, lastName, idNumber, email }) {
-    const user = {
-        firstName,
-        lastName,
-        username: idNumber,
-        email,
-        isDummy: true,
-        groups: ['student'],
-    };
-    newMutation({
-        collection: Users,
-        document: user,
-        validate: false,
-    });
+const createUser = function({ firstName, lastName, idNumber, email }) {
+  const user = {
+    firstName,
+    lastName,
+    username: idNumber,
+    email,
+    emails: [{ address: email, verified: false }],
+    groups: ['student'],
+  };
+  const userResult = newMutation({
+    collection: Users,
+    document: user,
+    validate: false,
+  });
+  Accounts.setPassword(userResult._id, email);
 };
 
-const importUsersFromFile = (data) => {
-    const wb = XLSX.read(data, { type: 'binary' });
-    /* Get first worksheet */
-    const wsname = wb.SheetNames[0];
-    const ws = wb.Sheets[wsname];
-    /* Convert array of arrays */
-    const result = XLSX.utils.sheet_to_json(ws, { header: 1 });
-    /* Create users */
-    result.forEach((user, index) => {
-        if (index !== 0) {
-            const [firstName, lastName, idNumber, email] = user;
-            createUser({ firstName, lastName, idNumber, email })
-        }
-    });
+const importUsersFromFile = data => {
+  const wb = XLSX.read(data, { type: 'binary' });
+  /* Get first worksheet */
+  const wsname = wb.SheetNames[0];
+  const ws = wb.Sheets[wsname];
+  /* Convert array of arrays */
+  const result = XLSX.utils.sheet_to_json(ws, { header: 1 });
+  /* Create users */
+  result.forEach((user, index) => {
+    if (index !== 0) {
+      const [firstName, lastName, idNumber, email] = user;
+      createUser({ firstName, lastName, idNumber, email });
+    }
+  });
 
-    return true;
-}
+  return true;
+};
 
 Meteor.methods({
-    'importUsersFromFile'({ data }) {
-        return importUsersFromFile(data);
-    }
-})
+  importUsersFromFile: ({ data }) => importUsersFromFile(data),
+});
